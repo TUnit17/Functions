d83ec502f3651038fdc4dba9f7a2468399a0840d
TUnit17 <thuang177@gmail.com>
2017-02-22 07:10:38 -0800

    inout
    This lets you modify the input variable directly

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -17,6 +17,7 @@ var width2 = 12
 var area2 = length2*width2
 
 
+// -> this means return
 func calculateArea(length:Int, width:Int)->Int
 {
 let area = length*width
@@ -25,3 +26,66 @@ let area = length*width
 }
 
 let shape1 = calculateArea(length: 5, width: 4)
+let shape2 = calculateArea(length: 5, width: 3)
+let shape3 = calculateArea(length: 5, width: 2)
+
+var bankAccountBalance = 500.00
+var shoes = 350.00
+
+
+/**
+// subtract itemPrice from currentBalance if you have enough money
+func purchaseItem(currentBalance:Double, itemPrice:Double)->Double
+{
+    if itemPrice <= currentBalance
+    {
+        print("Purchased item for $\(itemPrice)")
+        return currentBalance - itemPrice
+    }
+    else
+    {
+        print("YOur are broke. You should learn to save money.")
+        return currentBalance
+    }
+
+}
+bankAccountBalance = purchaseItem(currentBalance: bankAccountBalance, itemPrice: shoes)
+
+var lunchBox = 40.00
+
+bankAccountBalance = purchaseItem(currentBalance: bankAccountBalance, itemPrice: lunchBox)
+**/
+
+
+
+
+// inout = modify the original variable directly
+// subtract itemPrice from currentBalance if you have enough money
+ func purchaseItem(currentBalance:inout Double, itemPrice:Double)
+ {
+     if itemPrice <= currentBalance
+     {
+        currentBalance = currentBalance-itemPrice
+        print("Purchased item for $\(itemPrice)")
+         //return currentBalance - itemPrice
+        
+     }
+     else
+     {
+         print("YOur are broke. You should learn to save money.")
+         //return currentBalance
+     }
+ 
+ }
+ 
+ 
+ 
+ purchaseItem(currentBalance: &bankAccountBalance, itemPrice: shoes)
+ 
+ var lunchBox = 40.00
+ 
+purchaseItem(currentBalance: &bankAccountBalance, itemPrice: lunchBox)
+
+
+
+
